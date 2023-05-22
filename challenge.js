4a843822023102a2148f2834a98882f412cbaa08
Ismail2hussein <ihusseinmohamed629@gmail.com>
2023-05-21 21:50:02 -0700

    Update challenge.js

--- challenge.js
+++ challenge.js
@@ -1,2 +1,12 @@
 // Good Luck! You got this 💪🏾
 // Write your code here.
+let bill = 275
+let tip
+
+if (bill >= 50 && bill <= 300) {
+		tip = bill * 0.15
+    console.log(`The bill was ${bill}, the tip was ${tip}, and the total value ${bill + tip}`);
+} else if (bill > 300) {
+    tip = bill * 0.2
+    console.log(`The bill was ${bill}, the tip was ${tip}, and the total value ${bill + tip} `);
+}
